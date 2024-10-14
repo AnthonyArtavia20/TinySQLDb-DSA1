341f8c474db01cb87a30a4f80d5d2091dcd36f5b
Rachel <166075283+PFRachel@users.noreply.github.com>
2024-10-14 12:19:38 -0600

    Btree

--- ApiInterface/Processors/SQLSentenceProcessor.cs
+++ ApiInterface/Processors/SQLSentenceProcessor.cs
@@ -9,12 +9,10 @@ namespace ApiInterface.Processors
     internal class SQLSentenceProcessor : IProcessor //Permite dirigir los tipos de consultas a diferentes procesadores.
     {//en este caso permite dirigir las solicitudes SQLSentence al SQLQueryProcessor.
         public Request Request { get; }
-
         public SQLSentenceProcessor(Request request) //Realiza automáticamente el proceso para ejecutar las operaciones tipo SQL
         {
             Request = request;
         }
-
         public Response Process()
         {
             var sentence = this.Request.RequestBody.Trim(); //Se añade ".Trim" para evitar que la respuesta lleve espacios.
@@ -23,7 +21,6 @@ namespace ApiInterface.Processors
             return this.ConvertToResponse(status, data); //Finalmente se retorna Serealizado como tipo Data,
             //para que pueda ser enviado por el Socket
         }
-
         private Response ConvertToResponse(OperationStatus status, string data) //Cuando ya se procesó la solicitud, la procesamos 
         {//para poder pasarla como respuesta atravez del socket.
             var response = new Response
